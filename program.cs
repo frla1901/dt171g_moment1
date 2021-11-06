196b2a59be951e7e3f7486d43361eb67ad426428
frla1901 <frla1901@student.miun.se>
2021-11-06 09:55:17 +0100

    Commit changes of method Main

--- program.cs
+++ program.cs
@@ -6,10 +6,12 @@ namespace dt171g_moment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
+            Console.WriteLine("Vad heter du?");
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.WriteLine($"{Environment.NewLine}Dagens datum är: {currentDate:d}");
+            Console.WriteLine($"{Environment.NewLine}Klockan är: {currentDate:t}");
+            Console.WriteLine($"{Environment.NewLine}Ha en bra dag {name}!");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
         }
